64fdf2a7699f2d1c379c5341dbf2def9d93570d4
Tifany <tifsnunes@gmail.com>
2025-03-12 11:06:40 -0300

    add consuming the api and calling it in main

--- src/main/java/com/example/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/example/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.example.screenmatch;
 
+import com.example.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Teste");
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
 	}
 }
